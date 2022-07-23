492e7a5a7492c2c0a2d501f6451ca8ff7b5abc0e
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-07-23 22:41:32 +0530

    Adding Spring Config

--- pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
@@ -15,9 +15,9 @@ public class DataLoader implements CommandLineRunner {
     public final OwnerService ownerService;
     public final VetService vetService;
 
-    public DataLoader(){
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
