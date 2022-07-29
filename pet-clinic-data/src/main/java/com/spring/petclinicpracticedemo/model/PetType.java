b2baee868859d42857695a3d31822dcac7ecb468
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-07-29 20:13:38 +0530

    adding jpa to models.

--- pet-clinic-data/src/main/java/com/spring/petclinicpracticedemo/model/PetType.java
+++ pet-clinic-data/src/main/java/com/spring/petclinicpracticedemo/model/PetType.java
@@ -1,7 +1,11 @@
 package com.spring.petclinicpracticedemo.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
     
     private String name;
--- pet-clinic-data/src/main/java/com/spring/petclinicpracticedemo/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/spring/petclinicpracticedemo/model/Speciality.java
@@ -1,12 +1,20 @@
 package com.spring.petclinicpracticedemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
