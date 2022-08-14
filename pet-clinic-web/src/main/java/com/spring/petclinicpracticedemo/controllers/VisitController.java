88ff9bd63e0dec2125cda86d0b2a591cfa966624
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-15 00:54:18 +0530

    add methods dataBinder, initNewVisitForm, processNewVisitForm,

--- pet-clinic-web/src/main/java/com/spring/petclinicpracticedemo/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinicpracticedemo/controllers/VisitController.java
@@ -1,8 +1,18 @@
 package com.spring.petclinicpracticedemo.controllers;
 
+import com.spring.petclinicpracticedemo.model.Pet;
+import com.spring.petclinicpracticedemo.model.Visit;
 import com.spring.petclinicpracticedemo.services.PetService;
 import com.spring.petclinicpracticedemo.services.VisitService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.util.Map;
 
 
 @Controller
@@ -14,5 +24,43 @@ public class VisitController {
     public VisitController(VisitService visitService, PetService petService) {
         this.visitService = visitService;
         this.petService = petService;
+    }
+
+    @InitBinder
+    public void dataBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        Pet pet = petService.findById(petId);
+        model.put("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+
+            return "redirect:/owners/{ownerId}";
+        }
     }
 }
