cee2e619cbe08f86e3a3b400989c744e151a7d52
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-15 02:01:50 +0530

    rename class

--- pet-clinic-web/src/main/java/com/spring/petclinicpracticedemo/PetClinicPracticeDemoApplication.java
+++ pet-clinic-web/src/main/java/com/spring/petclinicpracticedemo/PetClinicPracticeDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetclinicApplication {
+public class PetClinicPracticeDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+		SpringApplication.run(PetClinicPracticeDemoApplication.class, args);
 	}
 
 }
