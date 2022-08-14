4deff2b5458124f85e3d2aa413250d8fcc88a49b
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-14 19:58:48 +0530

    add test class PetControllerTest

---
+++ pet-clinic-web/src/test/java/com/spring/petclinicpracticedemo/controllers/PetControllerTest.java
@@ -0,0 +1,14 @@
+package com.spring.petclinicpracticedemo.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+}
